3936bb174fd71427c2e38b6cfab8c3015ebd3873
Pedro Sanzio <72276805+phsanzio@users.noreply.github.com>
2023-01-04 14:49:58 +0000

    main

--- static/script.js
+++ static/script.js
@@ -13,3 +13,14 @@ function moveMenuLateral() {
   }
   body.classList.toggle("menu-nav-active");
 }
+
+const navItem = document.querySelectorAll(".nav-item");
+
+navItem.forEach((item) => {
+  item.addEventListener("click", () => {
+    if (body.classList.contains("menu-nav-active")) {
+      body.classList.remove("menu-nav-active");
+      menuMobile.classList.replace("bi-x", "bi-list");
+    }
+  });
+});
